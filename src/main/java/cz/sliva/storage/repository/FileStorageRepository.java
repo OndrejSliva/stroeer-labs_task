ac2d93d072b2d2d2dd3e4b1af7ae6c3d131a184f
Ondřej Slíva <Ondrej-Sliva@seznam.cz>
2022-10-16 14:41:19 +0200

    added implementation for FileStorageRepository

--- src/main/java/cz/sliva/storage/repository/FileStorageRepository.java
+++ src/main/java/cz/sliva/storage/repository/FileStorageRepository.java
@@ -1,12 +1,28 @@
 package cz.sliva.storage.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Repository
 public class FileStorageRepository implements StorageRepository {
 
+    private final String filePath;
+
+    public FileStorageRepository(@Value("${storage.flat-file.path}") final String filePath) {
+        this.filePath = filePath;
+    }
+
     @Override
     public void store(final String jsonString) {
-
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.filePath, true))) {
+            writer.write(jsonString);
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/cz/sliva/storage/repository/FileStorageRepositoryTest.java
@@ -0,0 +1,57 @@
+package cz.sliva.storage.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileStorageRepositoryTest {
+
+    private final String FIRST_JSON = "{\"foo\": \"bar\"}";
+    private final String SECOND_JSON = "{\"another\": \"json\"}";
+
+    @TempDir
+    private Path outputDir;
+    private Path outputFile;
+    private FileStorageRepository fileStorageRepository;
+
+    @BeforeEach
+    public void init() {
+        outputFile = outputDir.resolve("outputFile.json");
+        fileStorageRepository = new FileStorageRepository(outputFile.toString());
+    }
+
+    @Test
+    public void contentShouldBeStoredToFile() throws IOException {
+        assertTrue(Files.exists(outputDir));
+        assertFalse(Files.exists(outputFile));
+
+        fileStorageRepository.store(FIRST_JSON);
+
+        assertTrue(Files.exists(outputFile));
+
+        final String fileContent = Files.readAllLines(outputFile).get(0);
+        assertEquals(FIRST_JSON, fileContent);
+    }
+
+    @Test
+    public void anotherContentShouldBeAppendedToFile() throws IOException {
+        assertTrue(Files.exists(outputDir));
+        assertFalse(Files.exists(outputFile));
+
+        fileStorageRepository.store(FIRST_JSON);
+        fileStorageRepository.store(SECOND_JSON);
+
+        assertTrue(Files.exists(outputFile));
+
+        final String firstLine = Files.readAllLines(outputFile).get(0);
+        final String secondLine = Files.readAllLines(outputFile).get(1);
+        assertEquals(FIRST_JSON, firstLine);
+        assertEquals(SECOND_JSON, secondLine);
+    }
+}
